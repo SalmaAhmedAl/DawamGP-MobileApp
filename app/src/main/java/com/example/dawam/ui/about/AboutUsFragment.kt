427ffeb10f38d564fca9ac8380fdfcf6533e5b46
SalmaAhmedAli <salmahamzaahmed77@gmail.com>
2023-02-27 21:36:19 +0200

    3 fragments

--- app/src/main/java/com/example/dawam/ui/about/AboutUsFragment.kt
+++ app/src/main/java/com/example/dawam/ui/about/AboutUsFragment.kt
@@ -1,6 +1,21 @@
 package com.example.dawam.ui.about
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.dawam.databinding.FragmentAboutUsBinding
 
 class AboutUsFragment :Fragment(){
+    lateinit var viewBinding: FragmentAboutUsBinding
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+       viewBinding= FragmentAboutUsBinding.inflate(inflater, container, false)
+        return viewBinding.root
+    }
+
 }
--- app/src/main/java/com/example/dawam/ui/contactUs/ContactUsFragment.kt
+++ app/src/main/java/com/example/dawam/ui/contactUs/ContactUsFragment.kt
@@ -1,6 +1,20 @@
 package com.example.dawam.ui.contactUs
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.dawam.databinding.FragmentContactUsBinding
 
 class ContactUsFragment :Fragment(){
+    lateinit var viewBinding: FragmentContactUsBinding
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        viewBinding= FragmentContactUsBinding.inflate(inflater, container, false)
+        return viewBinding.root
+    }
 }
