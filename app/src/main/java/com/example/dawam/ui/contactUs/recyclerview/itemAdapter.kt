5034b6499eff87c7f442cd5e866fda4f07163840
SalmaAhmedAli <salmahamzaahmed77@gmail.com>
2023-02-27 21:42:20 +0200

    error solved

--- app/src/main/java/com/example/dawam/ui/contactUs/recyclerview/itemAdapter.kt
+++ app/src/main/java/com/example/dawam/ui/contactUs/recyclerview/itemAdapter.kt
@@ -1,27 +1,20 @@
 package com.example.dawam.ui.contactUs.recyclerview
 
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
 import androidx.recyclerview.widget.RecyclerView
-import androidx.recyclerview.widget.RecyclerView.Adapter
-import androidx.recyclerview.widget.RecyclerView.ViewHolder
-import com.example.dawam.R
 import com.example.dawam.databinding.ItemDesignBinding
-import com.example.dawam.databinding.ItemWaqfBinding
-import com.example.dawam.ui.home.WaqfAdapter
 
-class itemAdapter(val items :MutableList<itemDM>):RecyclerView.Adapter<itemAdapter.viewHolder>(){
-    class viewHolder (val viewBinding: ItemDesignBinding):RecyclerView.ViewHolder(viewBinding.root)
+class ItemAdapter(val items :MutableList<itemDM>):RecyclerView.Adapter<ItemAdapter.ViewHolder>(){
+    class ViewHolder (val viewBinding: ItemDesignBinding):RecyclerView.ViewHolder(viewBinding.root)
 
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): viewHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val viewBinding = ItemDesignBinding.inflate(LayoutInflater.from(parent.context), parent, false)
-        return itemAdapter.viewHolder(viewBinding)
+        return ViewHolder(viewBinding)
     }
 
-    override fun onBindViewHolder(holder: viewHolder, position: Int) {
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val item =items.get(position)
         holder.viewBinding.text.text=item.contactText
         holder.viewBinding.icon.setImageResource(item.imageId)
