3568e263dafbffda36c12ebd50d6fc50550a62e6
hager <https://github.com/hagarabobakr/islami_hager_abobakr.git>
2023-03-14 06:55:19 +0200

    + view model + data binding in submit fragment

--- app/src/main/java/com/example/dawam/ui/submit/SubmitRequestFragment.kt
+++ app/src/main/java/com/example/dawam/ui/submit/SubmitRequestFragment.kt
@@ -4,11 +4,15 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.example.dawam.R
 import com.example.dawam.databinding.FragmentSubmitRequestBinding
 
 
 class SubmitRequestFragment : Fragment() {
+    lateinit var viewModel: SubmitRequestViewModel
     lateinit var viewBinding: FragmentSubmitRequestBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,8 +22,14 @@ class SubmitRequestFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        viewBinding = FragmentSubmitRequestBinding.inflate(inflater, container, false)
+        viewBinding = DataBindingUtil.inflate(
+            inflater,
+            R.layout.fragment_submit_request, container, false
+        )
         return viewBinding.root
+
+        viewModel = ViewModelProvider(this).get(SubmitRequestViewModel::class.java)
+        viewBinding.vm = viewModel
     }
 
 
---
+++ app/src/main/java/com/example/dawam/ui/submit/SubmitRequestViewModel.kt
@@ -0,0 +1,9 @@
+package com.example.dawam.ui.submit
+
+import androidx.lifecycle.ViewModel
+
+class SubmitRequestViewModel : ViewModel() {
+    fun regester() {
+
+    }
+}
