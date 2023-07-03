c3ef189cd9d9c618d1585a7683319e7f5802a9c7
Salma Ahmed <salmahamzaahmed77@gmail.com>
2023-07-04 00:59:17 +0300

    Waqf add request completed!!

--- app/src/main/java/com/example/dawam/api/WebServices.kt
+++ app/src/main/java/com/example/dawam/api/WebServices.kt
@@ -1,6 +1,10 @@
 package com.example.dawam.api
 
+import com.example.dawam.api.model.waqfActivitiesResponse.WaqfActivitiesResponse
+import com.example.dawam.api.model.waqfCitiesResponse.WaqfCitiesResponse
+import com.example.dawam.api.model.waqfCountriesResponse.WaqfCountriesResponse
 import com.example.dawam.api.model.waqfResponse.WaqfResponse
+import com.example.dawam.api.model.waqfTypesResponse.WaqfTypesResponse
 import com.example.dawam.api.model.wqfDetailsResponse.WaqfDetailsResponse
 import retrofit2.Call
 import retrofit2.http.GET
@@ -13,4 +17,16 @@ interface WebServices {
 
     @GET("api/Waqf/{id}")
     fun getWaqfById(@Path(value ="id") id:Int): Call<WaqfDetailsResponse>
+
+    @GET("api/Waqf/Types")
+    fun getWaqfTypes(): Call<ArrayList<WaqfTypesResponse>>
+
+    @GET("api/Waqf/Activities")
+    fun getWaqfActivities(): Call<ArrayList<WaqfActivitiesResponse>>
+
+    @GET("api/Country")
+    fun getCountries(): Call<ArrayList<WaqfCountriesResponse>>
+
+    @GET("api/City/{id}")
+    fun getCities(@Path(value ="id") id:Int): Call<ArrayList<WaqfCitiesResponse>>
 }
