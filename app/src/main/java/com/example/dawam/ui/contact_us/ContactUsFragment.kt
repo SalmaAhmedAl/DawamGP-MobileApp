dfb0717348de086db0194a16fad28abbdd551032
Hager <https://github.com/hagarabobakr>
2023-07-05 01:52:56 +0300

    text animation

--- app/src/main/java/com/example/dawam/ui/contact_us/ContactUsFragment.kt
+++ app/src/main/java/com/example/dawam/ui/contact_us/ContactUsFragment.kt
@@ -1,10 +1,14 @@
 package com.example.dawam.ui.contact_us
 
 import android.os.Bundle
+import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.Animation
+import android.view.animation.ScaleAnimation
 import androidx.fragment.app.Fragment
+import androidx.transition.Slide
 import com.example.dawam.R
 import com.example.dawam.databinding.FragmentContactUsBinding
 import com.example.dawam.ui.contact_us.recyclerview.ItemAdapter
@@ -54,5 +58,14 @@ class ContactUsFragment :Fragment(){
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         adpater=ItemAdapter(items)
         viewBinding.content.contactUsRecycler.adapter=adpater
+        var textView =viewBinding.title
+        val anim = ScaleAnimation(
+            0f, 1f, // Start and end values for the X axis scaling
+            0f, 1f, // Start and end values for the Y axis scaling
+            Animation.RELATIVE_TO_SELF, 0.5f, // Pivot point of X scaling
+            Animation.RELATIVE_TO_SELF, 0.5f // Pivot point of Y scaling
+        )
+        anim.duration = 500
+        textView.startAnimation(anim)
     }
 }
